5fef643c59bc4b5a4c451e420f16f5f192f52390
evgeny <eugeneim@gmail.com>
2015-08-20 22:13:38 +0300

    initial

--- example/src/example_compiled_po.erl
+++ example/src/example_compiled_po.erl
@@ -49,9 +49,6 @@ get_idx(N, <<"ru">>) ->
 				    false -> 2
 				  end
 			    end);
-get_idx(N, <<"es_BO">>) ->
-	to_integer(to_integer(N)
-			      =/= to_integer(1));
 get_idx(N, <<"es">>) ->
 	to_integer(to_integer(N)
 			      =/= to_integer(1));
@@ -64,7 +61,6 @@ get_record(Key, Locale) ->
 	case Key of		<<"Hello"/utf8>> ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = <<"Привет"/utf8>>, msgstr_n = {}, n_max = 0};
-				<<"es_BO">> -> #porec2{msgstr = <<""/utf8>>, msgstr_n = {}, n_max = 0};
 				<<"es">> -> #porec2{msgstr = <<"Hola"/utf8>>, msgstr_n = {}, n_max = 0};
 			_ -> undefined
 			end;
@@ -77,35 +73,30 @@ get_record(Key, Locale) ->
 		{<<"masc"/utf8>>, <<"cat"/utf8>>} ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = <<"кот"/utf8>>, msgstr_n = {}, n_max = 0};
-				<<"es_BO">> -> #porec2{msgstr = <<""/utf8>>, msgstr_n = {}, n_max = 0};
 				<<"es">> -> #porec2{msgstr = <<"El gato"/utf8>>, msgstr_n = {}, n_max = 0};
 			_ -> undefined
 			end;
 		<<"Hello ~B world!"/utf8>> ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = undefined, msgstr_n = {<<"Привет ~B мир!"/utf8>>,<<"Привет ~B мира!"/utf8>>,<<"Привет ~B миров!"/utf8>>}, n_max = 3};
-				<<"es_BO">> -> #porec2{msgstr = undefined, msgstr_n = {<<""/utf8>>,<<""/utf8>>}, n_max = 2};
 				<<"es">> -> #porec2{msgstr = undefined, msgstr_n = {<<"Hola ~B mundo!"/utf8>>,<<"Hola ~B mundos!"/utf8>>}, n_max = 2};
 			_ -> undefined
 			end;
 		<<"Hello world!"/utf8>> ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = undefined, msgstr_n = {<<"Привет {{ n }} мир!"/utf8>>,<<"Привет {{ n }} мира!"/utf8>>,<<"Привет {{ n }} миров!"/utf8>>}, n_max = 3};
-				<<"es_BO">> -> #porec2{msgstr = undefined, msgstr_n = {<<""/utf8>>,<<""/utf8>>}, n_max = 2};
 				<<"es">> -> #porec2{msgstr = undefined, msgstr_n = {<<"Hola mundo!"/utf8>>,<<"Hola {{ n }} mundos!"/utf8>>}, n_max = 2};
 			_ -> undefined
 			end;
 		<<"House"/utf8>> ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = <<"Дом"/utf8>>, msgstr_n = {}, n_max = 0};
-				<<"es_BO">> -> #porec2{msgstr = <<""/utf8>>, msgstr_n = {}, n_max = 0};
 				<<"es">> -> #porec2{msgstr = <<"La casa"/utf8>>, msgstr_n = {}, n_max = 0};
 			_ -> undefined
 			end;
 		{<<"fem"/utf8>>, <<"cat"/utf8>>} ->
 			case Locale of
 				<<"ru">> -> #porec2{msgstr = <<"кошка"/utf8>>, msgstr_n = {}, n_max = 0};
-				<<"es_BO">> -> #porec2{msgstr = <<""/utf8>>, msgstr_n = {}, n_max = 0};
 				<<"es">> -> #porec2{msgstr = <<"La gata"/utf8>>, msgstr_n = {}, n_max = 0};
 			_ -> undefined
 			end;
